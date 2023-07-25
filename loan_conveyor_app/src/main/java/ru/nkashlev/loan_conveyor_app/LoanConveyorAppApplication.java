6e5789ed7db0ce94cd3e11dcafbd73490b819fdd
nkashlev <kashlev-1996@mail.ru>
2023-07-25 20:43:10 +0300

    Initial commit

---
+++ loan_conveyor_app/src/main/java/ru/nkashlev/loan_conveyor_app/LoanConveyorAppApplication.java
@@ -0,0 +1,13 @@
+package ru.nkashlev.loan_conveyor_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanConveyorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanConveyorAppApplication.class, args);
+	}
+
+}
